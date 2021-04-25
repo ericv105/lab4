960a0bad0dcf34712356e77cb33e4ec61b438e76
xwiss <ericvelepucha@gmail.com>
2021-04-25 03:34:10 -0400

    launch + print path

--- scripts/astar.py
+++ scripts/astar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# import rospy
+import rospy
 import math
 import heapq
 from collections import deque
@@ -17,7 +17,6 @@ def g_score(start_pos, x):
 
 def coord_to_arr(x):
     return (round_reg(x[0] + 9), round_reg(-x[1] + 10))
-    # return ( round_reg(9 + x[1]), round_reg(10 - x[1]) )
 
 def arr_to_coord(x):
     return (x[0]-8.5, -(x[1]-9.5))
@@ -70,19 +69,17 @@ def main():
             for i in map_playground:
                 for j in i:
                     if j == 0:
-                        print(' ', end=' ')
+                        print(' '),
                     else:
-                        print(j, end=' ')
-                # new = [' ' if j == 0 else j for j in i]
-                # print('[%s]'%', '.join(map(str, new)))
-                print()
+                        print(j),
+                print
             return path
         
         neighbors = [
-            (cur[0], cur[1] - 1), # top nbr
-            (cur[0] + 1, cur[1]), # right nbr
-            (cur[0] - 1, cur[1]), # left nbr
-            (cur[0], cur[1] + 1) # bottom nbr
+            (int(cur[0]), int(cur[1] - 1)), # top nbr
+            (int(cur[0] + 1), int(cur[1])), # right nbr
+            (int(cur[0] - 1), int(cur[1])), # left nbr
+            (int(cur[0]), int(cur[1] + 1)) # bottom nbr
         ]
         for nbr in neighbors:
             tg_score = g_score(start, cur) + h_score(cur, nbr)
@@ -98,8 +95,9 @@ def main():
                 fdict[nbr] = tg_score + h_score(nbr, end)
                 prev_node[nbr] = cur
                 gdict[nbr] = tg_score
-                # if nbr not in [i[1] for i in open_set]:
                 heapq.heappush(open_set, (fdict[nbr], nbr))
 
 if __name__ == "__main__":
-    print(main())
+    rospy.init_node('algo')
+    main()
+    rospy.spin()
